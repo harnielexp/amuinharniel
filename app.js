0253e07f8efb2b2abd06412b30c47776dbaca949
harnielexp <88357287+harnielexp@users.noreply.github.com>
2021-09-03 18:49:06 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const p = document.getElementById('pg');
+const t = document.getElementById('tl');
+
+function open() {
+    p.textContent = 'Hello World';
+    p.style.color = 'red'
+
+    t.textContent= 'Javascript Title Generated';
+}
+
+
+
