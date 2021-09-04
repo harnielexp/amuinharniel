bf04963be43bbc9cdbf18b4180c3e4ff71288961
harnielexp <88357287+harnielexp@users.noreply.github.com>
2021-09-03 19:08:43 -0700

    Icon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const p = document.getElementById('pg');
 const t = document.getElementById('tl');
+const i = document.getElementById('icon');
 
 function open() {
     p.textContent = 'Hello World';
